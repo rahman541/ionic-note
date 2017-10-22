106c76ae04a116ba7973fa75fa57d3f910719e63
Rahman <rahmanromli@yahoo.com>
2017-10-22 16:50:01 +0800

    Configure apollo-server-express

--- server/express/index.js
+++ server/express/index.js
@@ -1,6 +1,13 @@
 require('dotenv').config()
 import express from 'express'
+import bodyParser from 'body-parser'
+import { graphqlExpress, graphiqlExpress } from 'apollo-server-express'
+import graphQLSchema from './graphQLSchema'
 
 const app = express()
+
+// bodyParser is needed just for POST.
+app.use('/graphql', bodyParser.json(), graphiqlExpress({ schema: graphQLSchema }))
+app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }))
 
 app.listen(process.env.EXPRESS_PORT, () => console.log(`Server started at http://127.0.0.1:${process.env.EXPRESS_PORT}`))
